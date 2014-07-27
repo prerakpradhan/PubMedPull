b52386bb64f04cc7ba2aa85a6977932344e8b00d
Nishant Sinha <nsinha@uw.edu>
2014-07-27 14:14:08 -0700

    Mofified scripts to get current date

--- dbsetup.py
+++ dbsetup.py
@@ -5,8 +5,8 @@ def getDbConnection(hostname,username,password):
     db_con = MySQLdb.connect(host=hostname,user=username,passwd=password)
     return db_con
     
-def setupDB():
-    db_con=getDbConnection("localhost","nishant","password")
+def setupDB(hostname,username,password,startdate):
+    db_con=getDbConnection(hostname,username,password)
     db_cursor=db_con.cursor()    
     db_cursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'PubMedRepository'");
     db_stat=db_cursor.fetchone()
@@ -15,6 +15,7 @@ def setupDB():
         for mysql_stmt in open('setup_db.sql'):
             if mysql_stmt.strip():
                 db_cursor.execute(mysql_stmt.strip())
+        db_cursor.execute("insert into current_date values (%s)" % startdate)
         
     db_cursor.close()
     db_con.close()
@@ -26,6 +27,13 @@ def getLastId():
     last_id=db_cursor.fetchone()
     db_cursor.close()
     db_con.close()
-    return last_id
+    return last_id[0]
     
-setupDB()
+def getLastInsertDate():
+    db_con=getDbConnection("localhost","nishant","password")
+    db_cursor=db_con.cursor()
+    db_cursor.execute("SELECT last_insert_date from currentdate");
+    last_insert_date=db_cursor.fetchone()
+    db_cursor.close()
+    db_con.close()
+    return last_insert_date[0]
