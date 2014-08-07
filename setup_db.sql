61479613ecbad41945a0cfc6829db85675e05ec0
Nishant Sinha <nsinha@uw.edu>
2014-08-06 21:25:00 -0700

    fixed data trunation of doi and authors

--- setup_db.sql
+++ setup_db.sql
@@ -1,5 +1,5 @@
 create database PubMedRepository;
 use PubMedRepository;
-create table if not exists article_meta(aid varchar(64), accession varchar(10),pmcid varchar(10),pmc_uid varchar(10),publisher_id varchar(50),pmid varchar(10),doi varchar(100),title_paper varchar(500),journal_id varchar(60),journal_title varchar(500),date_of_pub varchar(20),abstract longtext,authors varchar(5000),primary key (aid)) character set utf8 collate utf8_general_ci;
+create table if not exists article_meta(aid varchar(64), accession varchar(10),pmcid varchar(10),pmc_uid varchar(10),publisher_id varchar(50),pmid varchar(10),doi varchar(500),title_paper varchar(500),journal_id varchar(60),journal_title varchar(500),date_of_pub varchar(20),abstract longtext,authors varchar(8000),primary key (aid)) character set utf8 collate utf8_general_ci;
 create table if not exists article_references(rid varchar(64),refid varchar(100),aid varchar(64),title varchar(2000),authors_list varchar(5000),pmid varchar(10),primary key(rid),foreign key (aid) references article_meta(aid)) character set utf8 collate utf8_general_ci;
 create table if not exists currentdate(last_insert_date date) character set utf8 collate utf8_general_ci;
